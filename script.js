09f6a3399b2acad383d8df1515e24de9b03c3b9b
Rohit <102841710+Rohit9324@users.noreply.github.com>
2024-07-21 18:12:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+var typeData = new Typed(".quality-value",{
+    strings: [
+      "Front-End Web Developer",
+      "Back-End Web Developer",
+      "Competitive Programmer",
+      "Student",
+    ],
+    loop:true,
+    typeSpeed: 100,
+    backSpeed:80,
+    backDelay:1000
+  });
+
+  
